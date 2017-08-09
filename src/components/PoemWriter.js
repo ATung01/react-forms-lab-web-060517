8cae3581372273f92b217689541e9a6260545f7d
github username <github email address>
2017-08-09 13:38:43 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,22 +4,63 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      text: "",
+      errorCheck: "This poem is not written in the right structure!"
+    };
   }
+
+
+
+  checkPoem = (poem) => {
+    poem = poem.split(/\n/)
+    let threeLines = poem.map(function (line) {
+      return line.trim()
+    })
+    console.log(threeLines)
+    console.log(threeLines.length)
+    if (threeLines.length === 3) {
+      if (threeLines[0].split(" ").length === 5 &&
+          threeLines[1].split(" ").length === 3 &&
+          threeLines[2].split(" ").length === 5) {
+
+            return true
+      }
+      else {
+          return false
+      }
+
+    }
+
+  }
+
+
+
+  updateText = (event) => {
+    let currentText = ""
+    this.setState({
+      text: event.target.value
+    }, () => {return currentText = this.state.text} )
+  }
+
 
   render() {
     return (
       <div>
-        <textarea 
-          rows="3" 
-          cols="60" 
+        <textarea
+          rows="3"
+          cols="60"
+          value={this.state.text}
+          onChange={this.updateText}
         />
-        <div 
-          id="poem-validation-error" 
+
+        {this.checkPoem(this.state.text) ? null : <div
+          id="poem-validation-error"
           style={{color: 'red'}}
-        >
-          This poem is not written in the right structure!
+        >"This poem is not written in the right structure!"
         </div>
+      }
+
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,32 @@
 import React from 'react';
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      numberOfChars: props.maxChars,
+      value: ""
+    };
   }
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value,
+      numberOfChars: (this.state.numberOfChars - 1)
+    })
+    }
+
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+
+        <p> {this.props.maxChars} is your max</p>
+        <p> {this.state.numberOfChars} is what you have left</p>
       </div>
     );
   }
